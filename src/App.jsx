0f11bb4825e3666d9f7891d2cc0dcbda9fdc0f63
Asaduzzaman04 <asadchy152@gmail.com>
2024-09-09 02:27:11 +0600

    unique%id%generate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Mdata from "./Components/mapping/Mdata";
-
 
 const App = () => {
  
   return (
     <>
-      <div className="bg-gray-900 text-yellow-100 h-[100vh] flex flex-col gap-6 justify-center items-center ">
-        <Mdata/>
+      <div className="bg-gray-900 text-yellow-100 h-[100vh] flex  gap-6 justify-center items-center  md:flex-row">
       </div>
     </>
   );
