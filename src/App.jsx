cee95da8346b5fb4df71b66a6555507b5a89ea67
Asaduzzaman04 <asadchy152@gmail.com>
2024-09-10 22:29:00 +0600

    use-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
+import State from './State/State';
 const App = () => {
  
   return (
     <>
       <div className="bg-gray-900 text-yellow-100 h-[100vh] flex  gap-6 justify-center items-center  md:flex-row">
- 
+          <State/>
       </div>
     </>
   );
