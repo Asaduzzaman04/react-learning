844bf8400d495bdc3f66ad9c3cc511730b717b17
Asaduzzaman04 <asadchy152@gmail.com>
2024-10-08 19:59:45 +0600

    timing function add

--- src/learnin-topic/Projects/TodoApp/List.jsx
+++ src/learnin-topic/Projects/TodoApp/List.jsx
@@ -1,32 +1,39 @@
+import { MdDelete } from "react-icons/md";
 import { useState } from "react";
 
+/* eslint-disable react/prop-types */
 
-const List = ({value}) => {
-    
-    const [taskDone , setTaskDone] = useState(false)
-   
-    const handleFill = () => {
-        setTaskDone(!taskDone)
-    
-      
-    }
 
-    return (
-        <>
-        <div onClick={handleFill} >
-            {
-                value.map((e, i) => {
-                    return (
-                        <>
-                        <p id={i} className={`border ${taskDone ? "line-through" : ""}`}>{e}</p>
-                        </>
-                    )
-                })
-            }
-        </div>
-            
-        </>
-    );
+const List = ({ value,deleted }) => {
+  const [taskDone, setTaskDone] = useState(false);
+
+  const handleFill = (e) => {
+   if(e.target.matches("p")){
+    setTaskDone(!taskDone);
+   }
+  };
+  
+
+  return (
+    <>
+      <div onClick={handleFill} className="mt-5 flex flex-col justify-center text-center items-center gap-2">
+        {value.map((e) => {
+          
+          return (
+            <>
+              <div className="flex rounded-lg justify-between bg-blue-900 w-full px-5 py-2">
+              <p   className={` capitalize ${taskDone ? "line-through" : ""}`}>
+                {e}
+              </p>
+              <button onClick={() => deleted(e)}> <MdDelete />
+              </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default List;
--- src/learnin-topic/Projects/TodoApp/Todo.jsx
+++ src/learnin-topic/Projects/TodoApp/Todo.jsx
@@ -1,40 +1,81 @@
-import { useState } from "react";
-import List from './List';
-
+import {  useState } from "react";
+import List from "./List";
 
 const Todo = () => {
-    const [value,setValue] = useState('')
- const [list,setList] = useState([])
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  // const [date, setDate] = useState();
+
+  const handleList = () => {
+    if (!value) return;
+    if (list.includes(value)) return;
+    setList((prevTask) => [...prevTask, value]);
+    setValue("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleDeleteTask = () => {
+    setList([]);
+  };
+
+  const handleDelete = (e) => {
+    console.log(e.target);
+  };
 
 
- const handleList = () => {
-    if(!value) return
-    setList((prevTask) => [...prevTask,value])
- }
 
 
- const handleSubmit = (e) => {
-        e.preventDefault()
- }
+  // useEffect(() =>{
+  //   const timerel = setInterval(() => {
+  //     const date = new Date()
+  //   const currentDate  = date.toLocaleDateString()
+  //   setDate(currentDate)
+  //   const  currentTime = date.toLocaleTimeString()
+  //   setTime(currentTime)
+  //   }, 1000);
 
-    return (
-        <>
-            <main className="border flex flex-col gap-10 justify-center items-center px-10 py-4">
-                <section>
-                    <h1 className="text-3xl font-bold">Todo app</h1>
-                </section>
-                <section>
-                    <form className="text-black" onSubmit={handleSubmit}>
-                        <div className="relative">
-                            <input className="border-none outline-none py-2 rounded-3xl px-4" type="text" spellCheck="false" placeholder="Enter your task" value={value} onChange={(event) => setValue(event.target.value)} />
-                        <button className="border text-white bg-orange-700 px-2  py-2 rounded-full absolute right-0  border-none " onClick={handleList}>add task</button>
-                        </div>
-                    </form>
-                    <List value={list} key={list.indexOf()}/>
-                </section>
-            </main>
-        </>
-    );
+  //   return () => clearInterval(timerel)
+  // },[])
+  
+  return (
+    <>
+      <main className="border rounded-lg flex flex-col gap-10 justify-center items-center px-10 py-4">
+        <section>
+          <h1 className="text-3xl font-bold">Todo app</h1>
+        </section>
+        {/* <section className="date_time">{date}</section> */}
+        <section>
+          <form className="text-black" onSubmit={handleSubmit}>
+            <div className="relative">
+              <input
+                className="border-none outline-none py-2 rounded-3xl px-4"
+                type="text"
+                spellCheck="false"
+                placeholder="Enter your task"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+              />
+              <button
+                className=" text-white bg-orange-700 px-2  py-2 rounded-full absolute -right-1  border-none "
+                onClick={handleList}
+              >
+                add task
+              </button>
+            </div>
+          </form>
+          <List value={list} deleted={handleDelete} />
+        </section>
+        <button
+          className={` capitalize text-lg px-2 py-1 rounded-md bg-red-500 font-bold`}
+          onClick={handleDeleteTask}
+        >
+          clear all
+        </button>
+      </main>
+    </>
+  );
 };
 
 export default Todo;
