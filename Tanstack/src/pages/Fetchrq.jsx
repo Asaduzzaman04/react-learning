f1db0bb03f3eee8ba26020a7f970c8d8dd868ec4
Asaduzzaman04 <asadchy152@gmail.com>
2024-11-08 20:08:08 +0600

    prefetch in tanstack

--- Tanstack/src/pages/Fetchrq.jsx
+++ Tanstack/src/pages/Fetchrq.jsx
@@ -22,10 +22,17 @@ const Fetchrq = () => {
 
     queryFn: handleTenData, //in tanstack queryfn works alternatively useEffect;
 
-    staleTime:10000, //In React Query, stale time defines how long fetched data remains fresh before being marked as stale and needing a refetch;
+    staleTime:10000,
+     //In React Query, stale time defines how long fetched data remains fresh before being marked as stale and needing a refetch;
 
-    gcTime :50000 , //In React Query, GC Time (controlled by cacheTime) specifies how long inactive query data stays cached before being garbage collected;
+    gcTime :50000 , 
+   //In React Query, GC Time (controlled by cacheTime) specifies how long inactive query data stays cached before being garbage collected;
+    
+    refetchInterval: 1000, 
+    //The `prefetchInterval` in TanStack Query specifies the time interval (in milliseconds) at which data will automatically be refetched to keep it updated;
 
+    refetchIntervalInBackground: true,
+     //it automatically be refetched to keep it updated in background by default false
 
   });
 
@@ -36,7 +43,7 @@ const Fetchrq = () => {
       </div>
 
       <div>
-        <ul className=" py-5 mt-10 gap-2 md:gap-4 lg:gap-8 px-2  grid justify-center  items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <ul className="py-5 mt-10 gap-2 md:gap-4 lg:gap-8 px-2  grid justify-center  items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {data?.map((item) => (
             <TanstackFetchCard
               key={item.id}
